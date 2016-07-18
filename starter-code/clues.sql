ed6c9d19371a3c41c6e397d1071a8584d8f61888
Luis Metzger <luismetzger@me.com>
2016-07-18 07:44:02 -0600

    Fixed capital answer for #6 in homework.

--- starter-code/clues.sql
+++ starter-code/clues.sql
@@ -47,9 +47,9 @@ SELECT * FROM country WHERE code='BRA';
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
 
-SELECT * FROM city WHERE id='265';
+SELECT * FROM city WHERE id='211';
 
- 265 | Serra | BRA         | Esp�rito Santo |     302666
+Brasília
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
  -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
